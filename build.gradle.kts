2d54e53a1f0717c3cf12eda74ad019b00169c786
vwagner <vwagner@figure.com>
2021-12-20 11:29:18 -0700

    GitHub action and publish (#7)
    * Adding publish workflow
    
    * WIP on github action build
    
    * add publishing step
    
    * add support for publishing to build
    
    * Add publish on release
    
    * publishing corrections
    
    * publishing corrections
    
    * add POM info for publication
    
    * added jar signing
    
    * correct publishing urls
    
    * add sources and javadocs to publishing
    
    * add sources and javadocs to publishing
    
    * temporarily specify version 0.0.1
    
    * prep for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
-//import Repos.sonatypeOss
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository
+import java.net.URI
 
 buildscript {
     repositories {
@@ -14,6 +16,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     `java-library`
     `maven-publish`
+    signing
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
@@ -26,7 +29,11 @@ repositories {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
+    withJavadocJar()
 }
+
+val projectVersion = project.property("version")?.takeIf { it != "unspecified" } ?: "1.0-SNAPSHOT"
 
 object Versions {
     val ProvenanceProtos = "1.7.0-0.0.2"
@@ -80,9 +87,74 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "io.provenance.client"
             artifactId = "pb-grpc-client-kotlin"
-            version = "1.0.0-SNAPSHOT"
+            version = "$projectVersion"
 
             from(components["java"])
+
+            pom {
+                name.set("Provenance Blockchain GRPC Kotlin Client")
+                description.set("A GRPC client for communicating with the Provenance Blockchain")
+                url.set("https://provenance.io")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("vwagner")
+                        name.set("Valerie Wagner")
+                        email.set("tech@figure.com")
+                    }
+                }
+
+                scm {
+                    connection.set("git@github.com:provenance-io/pb-grpc-client-kotlin.git")
+                    developerConnection.set("git@github.com/provenance-io/pb-grpc-client-kotlin.git")
+                    url.set("https://github.com/provenance-io/pb-grpc-client-kotlin")
+                }
+            }
+        }
+    }
+    signing {
+        sign(publishing.publications["maven"])
+    }
+}
+
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(findProject("ossrhUsername")?.toString() ?: System.getenv("OSSRH_USERNAME"))
+            password.set(findProject("ossrhPassword")?.toString() ?: System.getenv("OSSRH_PASSWORD"))
+            stagingProfileId.set("3180ca260b82a7") // prevents querying for the staging profile id, performance optimization
+        }
+    }
+}
+
+object Repos {
+    private object sonatype {
+        const val snapshots = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+        const val releases = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    }
+
+    fun RepositoryHandler.sonatypeOss(projectVersion: String): MavenArtifactRepository {
+        val murl =
+            if (projectVersion == "1.0-SNAPSHOT") sonatype.snapshots
+            else sonatype.releases
+
+        return maven {
+            name = "Sonatype"
+            url = URI.create(murl)
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
+            }
         }
     }
 }
