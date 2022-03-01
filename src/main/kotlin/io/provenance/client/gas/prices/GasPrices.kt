ad74727ccfe065d39f7383110ee69cf9aad352c1
Phil Story <mtps@users.noreply.github.com>
2022-03-01 16:10:48 -0700

    Add fallback pricing for gas estimations

--- src/main/kotlin/io/provenance/client/gas/prices/GasPrices.kt
+++ src/main/kotlin/io/provenance/client/gas/prices/GasPrices.kt
@@ -7,3 +7,17 @@ import kotlin.time.Duration.Companion.hours
 typealias GasPrices = () -> CoinOuterClass.Coin
 
 fun GasPrices.cached(ttl: Duration = 1.hours): GasPrices = CachedGasPrice(this, ttl)
+
+fun GasPrices.withFallbackPrice(gasPrice: CoinOuterClass.Coin): GasPrices {
+    val parent = this
+
+    return object : GasPrices {
+        override fun invoke(): CoinOuterClass.Coin {
+            return try {
+                parent.invoke()
+            } catch (e: Throwable) {
+                gasPrice
+            }
+        }
+    }
+}
