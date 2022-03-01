cd0d5118d60babae8a8a56bc6cf619b975332ebe
Phil Story <mtps@users.noreply.github.com>
2022-03-01 16:16:36 -0700

    Use cleaner syntax

--- src/main/kotlin/io/provenance/client/gas/prices/GasPrices.kt
+++ src/main/kotlin/io/provenance/client/gas/prices/GasPrices.kt
@@ -10,14 +10,5 @@ fun GasPrices.cached(ttl: Duration = 1.hours): GasPrices = CachedGasPrice(this, 
 
 fun GasPrices.withFallbackPrice(gasPrice: CoinOuterClass.Coin): GasPrices {
     val parent = this
-
-    return object : GasPrices {
-        override fun invoke(): CoinOuterClass.Coin {
-            return try {
-                parent.invoke()
-            } catch (e: Throwable) {
-                gasPrice
-            }
-        }
-    }
+    return { runCatching { parent.invoke() }.getOrDefault(gasPrice) }
 }
