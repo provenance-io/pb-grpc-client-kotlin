e4ba6ba98a2430f07e640f051373c41d635caf03
Phil Story <mtps@users.noreply.github.com>
2022-03-08 17:54:25 -0700

    Remove hardcoded NetworkType enum values

--- src/main/kotlin/io/provenance/client/wallet/NetworkType.kt
+++ src/main/kotlin/io/provenance/client/wallet/NetworkType.kt
@@ -1,8 +1,6 @@
 package io.provenance.client.wallet
 
-import io.provenance.hdwallet.hrp.Hrp
-
-enum class NetworkType(
+open class NetworkType(
     /**
      * The hrp (Human Readable Prefix) of the network address
      */
@@ -11,8 +9,4 @@ enum class NetworkType(
      * The HD wallet path
      */
     val path: String
-) {
-    TESTNET(prefix = Hrp.ProvenanceBlockchain.testnet, "m/44'/1'/0'/0/0'"),
-    COSMOS_TESTNET(prefix = Hrp.ProvenanceBlockchain.testnet, "m/44'/1'/0'/0/0"), // cause we are now creating validators on localnet with that path ffs
-    MAINNET(prefix = Hrp.ProvenanceBlockchain.mainnet, "m/505'/1'/0'/0/0")
-}
+)
