00b2259d20f1faf5a8c0f5259f01620f5be6e885
Phil Story <mtps@users.noreply.github.com>
2022-03-14 14:53:18 -0600

    Use big decimals to ensure amount stays out of scientific notation

--- src/main/kotlin/io/provenance/client/internal/extensions/CoinExtensions.kt
+++ src/main/kotlin/io/provenance/client/internal/extensions/CoinExtensions.kt
@@ -1,10 +1,11 @@
 package io.provenance.client.internal.extensions
 
 import cosmos.base.v1beta1.CoinOuterClass
+import java.math.BigDecimal
 
 internal fun Number.toCoin(denom: String): CoinOuterClass.Coin {
     return CoinOuterClass.Coin.newBuilder()
-        .setAmount(toString())
+        .setAmount(BigDecimal(toString()).toPlainString())
         .setDenom(denom)
         .build()
 }
@@ -15,17 +16,17 @@ internal fun String.toCoin(): CoinOuterClass.Coin {
     require(split > 0) { "invalid denom for coin:$this" }
 
     return CoinOuterClass.Coin.newBuilder()
-        .setAmount(substring(0, split))
+        .setAmount(BigDecimal(substring(0, split)).toPlainString())
         .setDenom(substring(split, length))
         .build()
 }
 
-internal operator fun List<CoinOuterClass.Coin>.times(other: Double): List<CoinOuterClass.Coin> = map { it * other }
+internal operator fun List<CoinOuterClass.Coin>.times(other: BigDecimal): List<CoinOuterClass.Coin> = map { it * other }
 
-internal operator fun CoinOuterClass.Coin.times(other: Double): CoinOuterClass.Coin {
+internal operator fun CoinOuterClass.Coin.times(other: BigDecimal): CoinOuterClass.Coin {
     return CoinOuterClass.Coin
         .newBuilder()
         .mergeFrom(this)
-        .setAmount((amount.toDouble() * other).toBigDecimal().toPlainString())
+        .setAmount(amount.toBigDecimal().times(other).toPlainString())
         .build()
 }
