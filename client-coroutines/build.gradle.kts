df5df3328d94086ee3378af39c99237804c2fea6
Wyatt <wbaker@figure.com>
2022-08-22 16:22:32 -0700

    Add simple test to coroutine library

--- client-coroutines/build.gradle.kts
+++ client-coroutines/build.gradle.kts
@@ -12,4 +12,8 @@ dependencies {
     implementation(libs.grpc.netty)
     implementation(libs.provenance.protos)
     implementation(libs.kotlinx.coroutines)
+
+    testImplementation(projects.client)
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
