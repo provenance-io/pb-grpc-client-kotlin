483405306c99a433232af84b5523e4918676ec22
Jason D <jdavidson@figure.com>
2025-06-30 10:42:19 -0600

    Deprecation error for floating gas price.

--- client/src/main/kotlin/io/provenance/client/gas/estimators/Floating.kt
+++ client/src/main/kotlin/io/provenance/client/gas/estimators/Floating.kt
@@ -1,20 +1,15 @@
 package io.provenance.client.gas.estimators
 
-import io.provenance.client.common.extensions.toCoin
 import io.provenance.client.common.gas.prices.GasPrices
 import io.provenance.client.grpc.GasEstimator
 import io.provenance.client.grpc.gasEstimator
 
+@Suppress("UNUSED_PARAMETER")
+@Deprecated(message= "As of 2.6.x and higher pb-grpc-client defaults to Provenance 1.25 flat fees and this function ignores the GasEstimator delegate fallback.", replaceWith = ReplaceWith("MSG_FEE_CALCULATION"), level = DeprecationLevel.WARNING)
 internal fun floatingGasPriceGasEstimator(delegate: GasEstimator, floatingGasPrice: GasPrices) = gasEstimator { tx, adjustment ->
-    val (price, denom) = floatingGasPrice().let { it.amount.toBigDecimal() to it.denom }
+    val (_, denom) = floatingGasPrice().let { it.amount.toBigDecimal() to it.denom }
     require(denom == "nhash") { "only nhash is supported for fees" }
 
-    // Original estimate
-    val estimate = delegate(tx, adjustment)
-
-    // Re-calculate price based on floating gas price instead of original.
-    val newBaseFee = listOf(price.times(estimate.limit.toBigDecimal()).toCoin(denom))
-
-    // Add in any message based fees to new base fee.
-    estimate.copy(feesCalculated = newBaseFee + estimate.msgFees)
+    // As of 2.6.x and higher default to Provenance 1.25 flat fee and ignore delegate
+    MsgFeeCalculationGasEstimator(tx, adjustment)
 }
